f700c552ec2b9fb74d8a537dfa1b09e2a13e00ee
Takashiro <takashiro@qq.com>
2018-06-02 20:51:26 +0800

    Initial commit

---
+++ src/gui/Lobby/index.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+class Lobby extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <div className="lobby">
+		</div>;
+	}
+
+}
+
+export default Lobby;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Lobby from './gui/Lobby';
+
+import './index.scss';
+
+ReactDOM.render(
+	<Lobby />,
+	document.getElementById('root')
+);
